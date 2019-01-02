d16bdcd03fdf053182b45cb9afe5300c910aff67
Jon Ribeiro <contact@jonathas.com>
2019-01-02 21:46:03 +0100

    Instalura: Implemented React Router

---
+++ part02/instalura/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+export default class Login extends Component {
+
+    constructor() {
+        super();
+        this.state = { msg: '' };
+    }
+
+    envia(event) {
+        event.preventDefault();
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({ login: this.login.value, senha: this.senha.value }),
+            headers: new Headers({
+                'Content-type': 'application/json'
+            })
+        };
+
+        fetch('http://localhost:8080/api/public/login', requestInfo)
+            .then(res => {
+                if (res.ok) {
+                    return res.text();
+                }
+                this.setState({ msg: 'Não foi possível fazer o login' });
+            })
+            .then(token => {
+                console.log(token);
+            });
+    }
+
+    render() {
+        return (
+            <div className="login-box">
+                <h1 className="header-logo">Instalura</h1>
+                <span>{this.state.msg}</span>
+                <form onSubmit={this.envia.bind(this)}>
+                    <input type="text" ref={(input) => this.login = input} />
+                    <input type="password" ref={(input) => this.senha = input} />
+                    <input type="submit" value="login" />
+                </form>
+            </div>
+        );
+    }
+}
--- part02/instalura/src/index.js
+++ part02/instalura/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './components/Login';
 import * as serviceWorker from './serviceWorker';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={Login} />
+            <Route path="/timeline" component={App} />
+        </div>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
